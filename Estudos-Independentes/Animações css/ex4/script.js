d26dfbbdd05963a957a088734949bca2e3e4b0e4
João Vitor Bastos <156615917+bastosjoaovitor@users.noreply.github.com>
2024-05-30 00:35:22 -0300

    Updated ex04 animation

--- Estudos-Independentes/Animações css/ex4/script.js
+++ Estudos-Independentes/Animações css/ex4/script.js
@@ -7,5 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
         const span = document.createElement('span');
         span.textContent = char;
         textElement.appendChild(span);
+
+        span.addEventListener('touchstart', function() {
+            this.classList.remove('not-touch');
+            this.classList.add('touch');
+        });
+
+        span.addEventListener('touchend', function() {
+            this.classList.remove('touch');
+            this.classList.add('not-touch');
+        });
     }
 });
